b1733cd7473c1b975c9fe68cd49672869dde5699
Jacob Wade <jacoblorenwade@Jacobs-MacBook-Pro.local>
2020-05-20 20:03:45 -0700

    Added filter code

--- static/js/app.js
+++ static/js/app.js
@@ -13,16 +13,39 @@ function buildTable(data) {
     // Create a for loop to go through each row of the array
     // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        
+
         // Append a row to the table body
         let row = tbody.append("tr");
 
         // Loop through each field in the data row and add
         // each value as a table cell (td)
-        Object.values(datarow).forEach((val) => {
+        Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
             cell.text(val);
             }
         );
     });
 }
+
+// Create a function to filter 
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if date was entered & filter data using date
+    if (date) {
+        // Apply filter to table data to only keep matchin rows
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    //@NOTE: If no date was entered then filteredData will just be original tableData
+    buildTable(filteredData);
+}
+
+// Function that handles a click
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Display unfiltered data immediately when loading page (especially first time)
+buildTable(tableData);
